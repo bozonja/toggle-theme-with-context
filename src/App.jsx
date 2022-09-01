9ec396ff232b643cbfc69eab14673e053b671cee
Josip Markovic <bozadesign@gmail.com>
2022-09-01 15:50:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+//comps
+import TopNavigation from "./components/TopNavigation.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TopNavigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TopNavigation = () => {
+  return (
+    <button type="button" onClick={() => {}}>
+      Change theme
+    </button>
+  );
+};
+
+export default TopNavigation;
