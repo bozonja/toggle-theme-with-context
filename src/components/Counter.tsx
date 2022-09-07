3b3449b22b35b4233de0a2f093778f83aea7bb65
Josip Markovic <bozadesign@gmail.com>
2022-09-07 16:42:17 +0200

    Change state with reducer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+// function init(initialCount) {
+//   return { count: initialCount };
+// }
+
+const initialState = { count: 0 };
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: 0 };
+    default:
+      throw new Error("");
+  }
+}
 
 export const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+
   return (
     <>
       <h4>This is counter:</h4>
-      <p>{count}</p>
-      <button type="button" onClick={() => setCount((c) => c + 1)}>
+      <p>{state.count}</p>
+      <button type="button" onClick={() => dispatch({ type: "increment" })}>
         +
       </button>
-      <button type="button" onClick={() => setCount((c) => c - 1)}>
+      <button type="button" onClick={() => dispatch({ type: "decrement" })}>
         -
       </button>
-      <button type="button" onClick={() => setCount(0)}>
+      <button type="button" onClick={() => dispatch({ type: "reset" })}>
         Reset
       </button>
     </>
